2a1a6761c2e000eba0a3d67bd2d45ac4886ab920
mustika chairani <mustikachrn5@gmail.com>
2023-08-18 16:18:09 +0700

    Create tantangan.dart

---
+++ 03_Fundamental Dart/Praktikum/tantangan.dart
@@ -0,0 +1,14 @@
+//buat variabel yang berfungsi untuk menghitung keliling persegi panjang dan segitiga
+
+ 
+int panjang = 10;
+int lebar = 4;
+int sisi_segitiga = 5;
+
+ int keliling_persegi_panjang = (2*panjang)+(2*lebar);
+ int keliling_segitiga = 3*sisi_segitiga;
+
+ void main(){
+  print('Keliling segitiga adalah $keliling_segitiga');
+  print('Keliling Persegi Panjang adalah $keliling_persegi_panjang');
+ }
