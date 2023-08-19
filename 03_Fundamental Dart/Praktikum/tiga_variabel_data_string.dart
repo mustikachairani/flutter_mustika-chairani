609fa94738c2639887cb05966fe35c19f5799c53
mustika chairani <mustikachrn5@gmail.com>
2023-08-20 01:04:23 +0700

    Create tiga_variabel_data_string.dart

---
+++ 03_Fundamental Dart/Praktikum/tiga_variabel_data_string.dart
@@ -0,0 +1,11 @@
+//membuat 3 buah variabel yang menampung tipe data String, lalu sambungkan 3 variabel tersebut dan tampilkan pada layar
+
+String var1 = "Halo";
+String var2 = "Selamat";
+String var3 = "Pagi";
+
+String tampil = var1 + "" + var2 + "" + var3;
+
+void main(){
+  print(" $tampil ");
+}
