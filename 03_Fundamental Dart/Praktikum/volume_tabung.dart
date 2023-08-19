bf5280b6d79e2153eabcef78f631a0dec9a04669
mustika chairani <mustikachrn5@gmail.com>
2023-08-20 01:12:03 +0700

    Create volume_tabung.dart

---
+++ 03_Fundamental Dart/Praktikum/volume_tabung.dart
@@ -0,0 +1,10 @@
+//rumus volume dari tabung (silinder)
+double jari = 14;
+double tinggi = 45;
+
+double volume_tabung = 3.14*jari*jari*tinggi;
+
+
+void main(){
+  print('Volume dari tabung adalah $volume_tabung');
+}
