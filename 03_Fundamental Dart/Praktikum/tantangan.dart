fef3935b08d3100e8f8f8ad85f89bd7b5c1a5fb6
mustika chairani <mustikachrn5@gmail.com>
2023-08-20 00:53:38 +0700

    Update tantangan.dart

--- 03_Fundamental Dart/Praktikum/tantangan.dart
+++ 03_Fundamental Dart/Praktikum/tantangan.dart
@@ -5,7 +5,7 @@ int panjang = 10;
 int lebar = 4;
 int sisi_segitiga = 5;
 
- int keliling_persegi_panjang = (2*panjang)+(2*lebar);
+ int keliling_persegi_panjang = 2*(panjang+lebar);
  int keliling_segitiga = 3*sisi_segitiga;
 
  void main(){
