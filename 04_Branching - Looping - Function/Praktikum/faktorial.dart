02510fa00a9a9e5b77bc95d13ec8bc7ff3c70ce3
mustika chairani <mustikachrn5@gmail.com>
2023-08-23 11:33:03 +0700

    Update faktorial.dart

--- 04_Branching - Looping - Function/Praktikum/faktorial.dart
+++ 04_Branching - Looping - Function/Praktikum/faktorial.dart
@@ -1,9 +1,12 @@
-void main(){
-  print("Faktorial dari 10 : ")
+void main() {
+  print("Faktorial dari 10 = ${faktorial(10)}");
+  print("Faktorial dari 40 = ${faktorial(40)}");
+  print("Faktorial dari 50 = ${faktorial(50)}");
 }
 
-BigInt Faktorial(int n) {
+BigInt faktorial(int n) {
   if (n == 0 || n == 1) {
-    return big
-  }
+    return BigInt.one;
+  } else {
+    return BigInt.from(n) * faktorial(n-1);}
 }
