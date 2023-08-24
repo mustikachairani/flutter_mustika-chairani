8169e66b03a1ae78c3091edea9345fa348166693
mustika chairani <mustikachrn5@gmail.com>
2023-08-24 11:20:04 +0700

    Create luas_lingkaran.dart

---
+++ 04_Branching - Looping - Function/Praktikum/luas_lingkaran.dart
@@ -0,0 +1,10 @@
+double luasLingkaran(double jariJari) {
+  double luas = 3.14 * jariJari;
+  return luas;
+}
+
+void main() {
+  double jariJari = 5;
+  double luas = luasLingkaran(jariJari);
+  print("Luas lingkaran dengan jari-jari $jariJari adalah $luas")
+}
