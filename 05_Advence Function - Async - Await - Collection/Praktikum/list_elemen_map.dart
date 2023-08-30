f665a42dd96b6291976fde3295aee3f33303825c
mustika chairani <mustikachrn5@gmail.com>
2023-08-30 13:41:26 +0700

    Update list_elemen_map.dart

--- 05_Advence Function - Async - Await - Collection/Praktikum/list_elemen_map.dart
+++ 05_Advence Function - Async - Await - Collection/Praktikum/list_elemen_map.dart
@@ -1,5 +1,12 @@
-void main(){
-  List<List<int>> listElenmen = {
-    
-  }
+void main() {
+  List<List<int>> listElemen = [
+    [1, 2],
+    [3, 4],
+    [5, 6],
+  ];
+
+  Map<int, List<int>> myMap = Map.fromIterable(listElemen,
+      key: (item) => listElemen.indexOf(item), value: (item) => item);
+
+  print(myMap);
 }
