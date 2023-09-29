619e6c6b9f8c335f4e0d951d8109af61b1c173ff
mustika chairani <mustikachrn5@gmail.com>
2023-09-29 22:06:48 +0700

    Create project weekly

--- Weekly1/Project/rani/lib/main.dart
+++ Weekly1/Project/rani/lib/main.dart
@@ -1,40 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:rani/welcome.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text('Rubah App')),
-        ),
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              expandedHeight: 200.0,
-              floating: false,
-              pinned: true,
-              flexibleSpace: FlexibleSpaceBar(
-                title: Text('Welcome to Section'),
-                background: Image.network('https://th.bing.com/th/id/OIP.a_Y-cHzWGMIwIMeLYMj6CwHaLH?w=186&h=279&c=7&r=0&o=5&pid=1.7'),
-                fit:Box,
-              ),
-            )
-          ],
-        ),
-      ),
+      home: Welcome()
+    
     );
   }
 }
